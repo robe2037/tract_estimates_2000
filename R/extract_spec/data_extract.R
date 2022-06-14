b704bdd5171ba5fe12eb8a2f6c09a102f184f108
Finn Roberts <robe2037@umn.edu>
2022-06-14 19:39:14 +0000

    add tracts to extracts

--- R/extract_spec/data_extract.R
+++ R/extract_spec/data_extract.R
@@ -4,7 +4,7 @@ library(tidyverse)
 
 # 2010-standardized 2000 + 2010 SF1a for Sex/Age/Race
 define_extract_nhgis(
-  description = "County counts for tract estimate pipeline: standardized",
+  description = "County and tract counts for tract estimate pipeline: standardized",
   time_series_tables = c(
     "CO7",
     "CO8", 
@@ -14,35 +14,23 @@ define_extract_nhgis(
     "CP2", 
     "CP3"
   ),
-  tst_geog_levels = "county",
+  tst_geog_levels = c("county", "tract"),
   tst_layout = "time_by_row_layout"
 ) %>% 
   submit_extract() %>%
   wait_for_extract() %>%
-  download_extract(here::here("data"))
+  download_extract(here::here("data", "extracts"))
 
 # 2000 and 2010 non-standardized boundaries at county level.
 define_extract_nhgis(
-  description = "Block counts for tract estimate pipeline",
+  description = "County and tract counts for tract estimate pipeline",
   datasets = c("2000_SF1a", "2010_SF1a"),
   ds_tables = list(
     "NP012D",
     c("P12A", "P12B", "P12C", "P12D", "P12E", "P12F", "P12G")
   ),
-  ds_geog_levels = "county"
+  ds_geog_levels = c("county", "tract")
 ) %>% 
   submit_extract() %>%
   wait_for_extract() %>%
-  download_extract(here::here("data"))
-
-# 2000 SF1b for block-level Sex/Age/Race
-define_extract_nhgis(
-  description = "Block counts for tract estimate pipeline",
-  datasets = c("2000_SF1a", "2000_SF1b"),
-  ds_tables = "NP012D",
-  ds_geog_levels = list("county", "block"),
-  geographic_extents = "020"
-) %>% 
-  submit_extract() %>%
-  wait_for_extract() %>%
-  download_extract(here::here("data"))
+  download_extract(here::here("data", "extracts"))
